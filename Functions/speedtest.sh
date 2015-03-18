7237f47b64d6aa9bf3888a208a2083272230c528
BlameTheNetwork <oncall@blamethenetwork.com>
2015-03-18 00:42:57 -0400

    Update speedtest.sh

--- Functions/speedtest.sh
+++ Functions/speedtest.sh
@@ -1,35 +1,57 @@
 ###
 ### NetPi Internet Speed Test Visual Script
-### Runs on speedtest-cli | developed by Matt Martz
-### github.com/sivel/speedtest-cli
-###
+##
+# Runs on speedtest-cli | developed by Matt Martz
+##
+# github.com/sivel/speedtest-cli
+#
+##
 
 #######################################
 # Define the SpeedTest Output as a var#
 #######################################
-dtstamp=$(date +"%m-%d-%Y--%H%M")
-speedtest=$(speedtest-cli | sed 1,2d)
+echo Working
+dtstamp=$(date +"%m-%d-%Y--%H%M%S")
+speedtest-cli | tee /share/logs/speedtest/report-$dtstamp.txt
+CONTENT=$(cat /share/logs/speedtest/report-$dtstamp.txt)
+
 
 #######################################
+
 # HTML Content // Edit to your needs  #
+
 #######################################
-open="<html><head><title>Speed Test Output</title><style type=text/css>BODY {
+
+open="<html><head><title>Speed Test Output</title>
+<style type=text/css>BODY {
 color:#FFFFFF; background-color:#000000;
-}</style></head><body><h1>NetPi Speed Test</h1><pre>"
-close="</pre><br/><input type='button' value='Save Report'/><input
-type='button' value='New Scan'/></body></html>"
+}</style></head>
+<body><h1>NetPi Speed Test</h1><pre>"
+
+close="</pre><br/>You may now close this window. The log was saved to the logs directory.</body></html>"
+
 
 ######################################
+
 # Write content to HTML file         #
+
 ######################################
+
 _file="/share/logs/speedtest"
-echo "$lldpdata" > "$_file/speedtestlog-$dtstamp.txt"
+
 echo "$open" > "$_file/speedtestreport.html"
-echo "$lldpdata" >> "$_file/speedtestreport.html"
-echo "$close" >> "$_file/speedtestreport.html"
+
+echo "$CONTENT" >> "$_file/speedtestreport.html"
+
+#echo "$close" >> "$_file/speedtestreport.html"
+
 
 #####################################
+
 #Open output in Dillo. Fastest load #
+
 #####################################
-dillo -f "$_file/speedtestreport.html"
-#####################################
+chromium-browser --app=file://$_file/speedtestreport.html
+#dillo -f "$_file/speedtestreport.html"
+
+######################################
