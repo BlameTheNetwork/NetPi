b117d783aa07166040b608b3cd25b5bcb60242f4
BlameTheNetwork <oncall@blamethenetwork.com>
2015-03-18 00:42:19 -0400

    Update netwake.sh

--- Functions/netwake.sh
+++ Functions/netwake.sh
@@ -1,8 +1,6 @@
-# Attempts to background ping to default wlan gateway to keep WLAN device from sleeping
-# Other attempts to turn off power management for WLAN appear unsuccessful. Even this script is questionable.
-# Appears to be an issue with driver support on RasPis. USB ports seem to have enough power
-# This is the best option for an attempted fix I can come up with.
-echo "Pinging default gateway to keep wireless lan alive. CTRL+C to exit, or exit terminal"
-echo "YOU CAN MIMINIZE THIS WINDOW AND LET IT RUN"
+echo *****************************
+echo  MINIMIZE THIS ACTIVE PING 
+echo   NetPi KEEPALIVE RUNNING! 
+echo *****************************
 gw="$(ip route show 0.0.0.0/0 dev wlan0 | cut -d\  -f3)"
-ping ${gw}
+ping $gw
